d9ba764d79585dfc897ce5aa77fb76372be0a955
olga.dubitskaya <o.n.dubitskaya@gmail.com>
2021-10-23 15:18:27 +0300

    server

--- server/server.js
+++ server/server.js
@@ -35,4 +35,4 @@ app.use(express.static(path.join(__dirname, "/../build")))
 app.get("*", (req, res) => {
 	res.sendFile(path.join(`${__dirname}/../build/index.html`))
 })
-http.createServer(app).listen()
+const server = app.listen()
